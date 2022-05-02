beb9a281cc7682fb0621f41aa9a8e2cb8dc198f3
Cream <Arisaraccream@gmail.com>
2022-05-02 14:22:15 +0700

    added server

---
+++ server/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+
+async function init() {
+  dotenv.config();
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    secure: true,
+  });
+
+  const app = express();
+  const port = 4000;
+
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
+
+init();
